824b27dade319182d6773f44c979e3d17517b5ec
Saide <ziaeefard.1@osu.edu>
2024-11-01 12:30:21 +0000

    Modified code for automation

--- Docs.py
+++ Docs.py
@@ -1,1 +1,8 @@
-need to automate the docuementation
+#need to automate the docuementation
+import os
+import subprocess
+
+def generate_docs():
+# Path to your project source code and documentation folders
+project_src = "path/to/your_project"
+docs_dir = "docs"
